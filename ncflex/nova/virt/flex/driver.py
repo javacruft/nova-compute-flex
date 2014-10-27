dd3ebcd2da0200e097a0d1654df482cdbb977c61
Chuck Short <chuck.short@canonical.com>
2014-10-27 10:40:56 -0400

    More typo fixes
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- ncflex/nova/virt/flex/driver.py
+++ ncflex/nova/virt/flex/driver.py
@@ -94,7 +94,7 @@ class LXCDriver(driver.ComputeDriver):
         self.containers.destroy_container(context, instance, network_info,
                                           block_device_info, destroy_disks)
     
-    def cleanup(self. context, instance, network_info, block_device_info=None,
+    def cleanup(self, context, instance, network_info, block_device_info=None,
                 destroy_disks=True, migrate_data=None, destroy_vifs=True):
         self.containers.cleanup_container(context, instance, network_info,
                                           block_device_info, destroy_disks,
